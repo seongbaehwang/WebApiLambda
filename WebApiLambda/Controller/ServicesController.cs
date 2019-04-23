422a6565c088107aaaa62b74c138daca6c571d3e
Seong Bae Hwang <seongbae_h@hotmail.com>
2019-04-23 20:14:37 +1000

    Add MVC support

---
+++ WebApiLambda/Controller/ServicesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApiLambda.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly IServiceCollection _services;
+
+        public ServicesController(IServiceCollection services)
+        {
+            _services = services;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _services.Select(sd => $"{sd.ServiceType} - {sd.ImplementationType}");
+        }
+    }
+}
