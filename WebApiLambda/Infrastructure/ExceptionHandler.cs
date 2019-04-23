b823ae5b3ed007b30f1a7700f8294ff4cbe86735
Seong Bae Hwang <seongbae_h@hotmail.com>
2019-04-23 22:02:54 +1000

    Added exception handler and cors

---
+++ WebApiLambda/Infrastructure/ExceptionHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace WebApiLambda.Infrastructure
+{
+    internal static class ExceptionHandler
+    {
+        public static RequestDelegate JsonResponeExceptionHandler = async context =>
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+            if (contextFeature != null)
+            {
+                var env = context.RequestServices.GetRequiredService<IHostingEnvironment>();
+
+                if (env.IsDevelopment())
+                {
+                    await WriteResponseBodyAsync(context, contextFeature.Error);
+                }
+                else
+                {
+                    await WriteResponseBodyAsync(context);
+                }
+            }
+        };
+
+        private static async Task WriteResponseBodyAsync(HttpContext context)
+        {
+            var mvcJsonOptions = context.RequestServices.GetRequiredService<IOptions<MvcJsonOptions>>().Value;
+
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(
+                new
+                {
+                    context.Response.StatusCode,
+                    Message = "Internal Server Error."
+                }, mvcJsonOptions.SerializerSettings));
+        }
+
+        private static async Task WriteResponseBodyAsync(HttpContext context, Exception error)
+        {
+            var mvcJsonOptions = context.RequestServices.GetRequiredService<IOptions<MvcJsonOptions>>().Value;
+
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(
+                new
+                {
+                    context.Response.StatusCode,
+                    error.Message,
+                    Error = error
+                }, mvcJsonOptions.SerializerSettings));
+        }
+    }
+}
--- WebApiLambda/Startup.cs
+++ WebApiLambda/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using WebApiLambda.Infrastructure;
 
 namespace WebApiLambda
 {
@@ -12,11 +13,30 @@ namespace WebApiLambda
             // TODO: This is for testing purpose
             services.AddSingleton(services);
 
+            services.Configure<ExceptionHandlerOptions>(options =>
+                options.ExceptionHandler = ExceptionHandler.JsonResponeExceptionHandler);
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder => builder.AllowAnyOrigin());
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseExceptionHandler();
+
+            if (!env.IsDevelopment())
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors();
+
+            app.UseHttpsRedirection();
+
             app.UseMvc();
         }
     }
